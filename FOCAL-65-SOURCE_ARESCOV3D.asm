f8d1c27479589fd869b4bdb7834cb3c6c1a7f6e6
Sam Colwell <scolwell@nfatech.com>
2024-02-11 16:11:49 -0500

    Removed zero page init patch and it now appears to work and is a binary match to Hans Otten's unpatched binary

--- FOCAL-65-SOURCE_ARESCOV3D.asm
+++ FOCAL-65-SOURCE_ARESCOV3D.asm
@@ -14,24 +14,21 @@
 
 ; SamCoVT - notes from focal 6502 user notes (describing a zero page
 ; initialization routine) indicate that only $00-$BF needs to be loaded.
-; This zero page data has been patched to make FOCAL65 take 8K (assumes
-; a 16K KIM-1).  This leaves room on the end for other patches, including
-; the aforementioned zero page initialization routine.
+        .ORG $0000
 
-;        .ORG $0000
-;
-;        .BYTE $53, $53, $4C, $E0, $2C, $4C, $DF, $2C, $00, $00, $00, $00, $00, $00, $00, $00        
-;        .BYTE $00, $62, $7B, $66, $EB, $6B, $3A, $7B, $6A, $6B, $6B, $7B, $FB, $7B, $6B, $6B
-;        .BYTE $00, $00, $00, $00, $00, $00, $FF, $00, $00, $01, $00, $00, $00, $00, $00, $E0
-;        .BYTE $3F, $FE, $3F, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $FF, $3F
-;        .BYTE $FF, $3F, $FF, $3F, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
-;        .BYTE $00, $00, $00, $00, $5F, $00, $00, $00, $00, $00, $00, $00, $00, $00, $4C, $00
-;        .BYTE $00, $6C, $00, $00, $48, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $C0
-;        .BYTE $10, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
-;        .BYTE $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $05
-;        .BYTE $05, $83, $50, $00, $00, $00, $7F, $40, $00, $00, $00, $80, $40, $00, $00, $00
-;        .BYTE $00, $00, $00, $00, $00, $FF, $1C, $3C, $7C, $5F, $7F, $7C, $3E, $7F, $3E, $FF
-;        .BYTE $94, $DD, $84, $D4, $85, $D4, $95, $04, $DD, $94, $95, $DF, $8C, $15, $1D, $94
+        .BYTE $53, $53, $4C, $E0, $2C, $4C, $DF, $2C, $00, $00, $00, $00, $00, $00, $00, $00        
+        .BYTE $00, $62, $7B, $66, $EB, $6B, $3A, $7B, $6A, $6B, $6B, $7B, $FB, $7B, $6B, $6B
+        .BYTE $00, $00, $00, $00, $00, $00, $FF, $00, $00, $01, $00, $00, $00, $00, $00, $EB
+        .BYTE $35, $09, $36, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $0A, $36
+        .BYTE $0A, $36, $0A, $36, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+        .BYTE $00, $00, $00, $00, $3F, $00, $00, $00, $00, $00, $00, $00, $00, $00, $4C, $00
+        .BYTE $00, $6C, $00, $00, $48, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $C0
+        .BYTE $10, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+        .BYTE $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $05
+        .BYTE $05, $83, $50, $00, $00, $00, $7F, $40, $00, $00, $00, $80, $40, $00, $00, $00
+        .BYTE $00, $00, $00, $00, $00, $FF, $1C, $3C, $7C, $5F, $7F, $7C, $3E, $7F, $3E, $FF
+        .BYTE $94, $DD, $84, $D4, $85, $D4, $95, $04, $DD, $94, $95, $DF, $8C, $15, $1D, $94
+
 ;        .BYTE $00, $D3, $E0, $C1, $80, $90, $D3, $C0, $C2, $A3, $00, $C0, $81, $82, $C3, $80
 ;        .BYTE $62, $6A, $FB, $6B, $6A, $6A, $02, $FF, $5A, $4B, $E3, $10, $EB, $62, $62, $4B
 ;        .BYTE $7F, $6C, $7F, $7D, $3F, $7F, $2E, $7F, $7F, $7E, $7F, $36, $5F, $7E, $3E, $7F
@@ -3874,54 +3871,12 @@ CLOAL     .BYTE <RTS1
           .BYTE 0
           .BYTE 0
 
-; SamCoVT - Zero page initialization routine from focal 6502 user notes
-; patched in here.
-ZPAGE = 0
-LENGTH = $BF            ; NUMBER OF BYTES
-STARTF = $2000
-        .ORG $3F00
-; CSTART (address $3F00) will now be the new cold start address.
-CSTART
-        LDX #0          ; INIT THE LOOP COUNTER
-ZLOOP
-        LDA ZSTORE,X    ; START MOVING DATA
-        STA ZPAGE,X
-        INX
-        CPX #LENGTH+1
-        BNE ZLOOP
-        JMP STARTF      ; PAGE IS SET UP
-                        ; GO TO FOCAL
-
-        .ORG $3F10
-ZSTORE
-        .BYTE $53, $53, $4C, $E0, $2C, $4C, $DF, $2C, $00, $00, $00, $00, $00, $00, $00, $00        
-        .BYTE $00, $62, $7B, $66, $EB, $6B, $3A, $7B, $6A, $6B, $6B, $7B, $FB, $7B, $6B, $6B
-        .BYTE $00, $00, $00, $00, $00, $00, $FF, $00, $00, $01, $00, $00, $00, $00, $00, $E0
-        .BYTE $3F, $FE, $3F, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $FF, $3F
-        .BYTE $FF, $3F, $FF, $3F, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
-        .BYTE $00, $00, $00, $00, $5F, $00, $00, $00, $00, $00, $00, $00, $00, $00, $4C, $00
-        .BYTE $00, $6C, $00, $00, $48, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $C0
-        .BYTE $10, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
-        .BYTE $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $05
-        .BYTE $05, $83, $50, $00, $00, $00, $7F, $40, $00, $00, $00, $80, $40, $00, $00, $00
-        .BYTE $00, $00, $00, $00, $00, $FF, $1C, $3C, $7C, $5F, $7F, $7C, $3E, $7F, $3E, $FF
-        .BYTE $94, $DD, $84, $D4, $85, $D4, $95, $04, $DD, $94, $95, $DF, $8C, $15, $1D, $94
-
-;
-;      FOCEND - TEXT AREAS AND THE LIKE
-;
-; SamCoVT - Zero page variables have been modified to make room
-; for zero page loading routine (extending FOCAL to take up a full
-; 8KB).  As a result, line 0.0 (below) needs to be moved up to
-; $3FE0.
-          .ORG $3FE0
 PRGBEG    .BYTE 0	; LINE NUMBER OF 00.00
           .BYTE 0     
           .ASCII  " C FOCAL-65 (V3D) 26-AUG-77"
           .BYTE $0D	; 'CR'
 
 
-PBEG     .BYTE EOP     ; START OF PROGRAM TEXT AREA
-VEND     .BYTE EOV     ; END OF VARIABLE LIST
-
+PBEG      .BYTE EOP     ; START OF PROGRAM TEXT AREA
+VEND      .BYTE EOV     ; END OF VARIABLE LIST
          .END     
--- README.md
+++ README.md
@@ -11,9 +11,8 @@ along with some history.
 
 Hans Otten also has an excellent simulator for the KIM-1 here:
 http://retro.hansotten.nl/6502-sbc/kim-1-manuals-and-software/kim-1-simulator/
-This software should run on the KIM-1 Simulator software but needs a
-special FOCAL setting turned on (details on both the FOCAL-65 and
-KIM-1 simulator pages).
+This software should run on the KIM-1 Simulator software but needs
+special FOCAL V3D break handline turned on in the settings.
 
 This is a separate effort from Hans Otten's work to recreate this code
 from the scanned listing.  This code has been formatted for the
@@ -22,19 +21,18 @@ only minor formatting or assembler directive changes.
 
 # Current Status:
 
-The source assembles but is not yet a binary match to the historical
-version.  Most of the source code typos have been eliminated.  
+The source assembles and is a binary match to the historical
+version provided by Hans Otten with the exception of extra bytes
+at the end of zero page and at the end of the program.
 
-I may also install the zero-page initialization patch.
-The sources for this patch (and other historical
-patches) are available on Hans Otten's FOCAL-65 page in the "KIM/6502
-User notes" section:
-http://retro.hansotten.nl/6502-sbc/focal-65-v3d/#articles
+This version appears to work in the KIM1 simulator (lightly tested).
 
-Hans Otten has also provided a "clean" version of the binary, as the
-version I am currently comparing to has extra unneeded bytes in zero
-page and on the end.  I will be switching to that binary version
-shortly.
+I tried adding the zero page initialization routine from the FOCAL
+6502 User Notes, but FOCAL does not run properly so that has been
+removed in this version.  There appear to be some addresses that are
+hard coded that need to be adjusted above an beyond the zero page
+values listed in the FOCAL 6502 User Notes.  The code for this patch
+has been placed in zpinit.asm.
 
 # Assembling Notes:
 Using v1.3.4.7 from https://sbc.rictor.org/kowalski.html
@@ -53,14 +51,16 @@ File (menu) -> Save code
 
 select format at bottom (Files of type: dropdown) and then provide file name
 
-Additional note for using KIM1 simulator - Motorola S-records appear to
-get all segments to load into the correct locations with a single file (Intel
-hex does not).
+Additional note for using KIM1 simulator - Motorola S-records appear
+to get all segments to load into the correct locations with a single
+file (Intel hex does not.  Binary dumps the entire 64K memory space
+and will overwrite hardware configuration registers for the KIM-1, 
+causing failure as well).
 
 
 # Binary comparison notes:
 ## Programs used:
-Assemble with Kowalski simulator (see above) - it's very important to uncheck the
+Assemble with Kowalski simulator (see above) - it's **very important** to uncheck the
 assembler option that generates an extra byte after the BRK instruction.
 
 Save code as "Binary image" to filename FOCALNEW.bin
@@ -79,20 +79,22 @@ dhex 0.69 used for binary comparison
 http://www.dettus.net/dhex/
 
 ## Filenames used:
+Original binary from Dave Hassler with some patches applied (I/O and 16K KIM1?):
+
 FOCAL-650000.bin - binary dump of zero page from PTP (papertape) file
 
 FOCAL-652000.bin - binary dump of program (starting at $2000) from PTP file
 
 FOCALNEW.bin - binary image (entire 64K memory space) from Kowalski assembler after assembling FOCAL-65-SOURCE_ARESCOV3D_RAW-WORK_V2.asm 
 
-Cleaner original binary from Hans Otten:
+Cleaner (no patches) original binary from Hans Otten:
 
 FOCALZP.BIN - binary dump of zero page
 
 FOCALM.BIN - binary dump of program (starting at $2000)
 
 ## Zero page compare
-dhex FOCALNEW.bin FOCAL-650000.bin
+dhex FOCALNEW.bin FOCALZP.bin
 
 ## Program compare
-dhex -a2h 2000 -o1h 2000 FOCALNEW.bin FOCAL-652000.bin
+dhex -a2h 2000 -o1h 2000 FOCALNEW.bin FOCALM.bin
